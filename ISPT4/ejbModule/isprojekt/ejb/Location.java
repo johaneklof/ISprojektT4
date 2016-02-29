496f26bab9b8321a8d412bd7d8a144099494d391
wengle <wengle@live.se>
2016-02-29 15:18:42 +0100

    Location döne

--- ISPT4/ejbModule/isprojekt/ejb/Location.java
+++ ISPT4/ejbModule/isprojekt/ejb/Location.java
@@ -2,4 +2,28 @@ package isprojekt.ejb;
 
 public class Location {
 
+	private String name;
+	private String address;
+	private String country;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
 }
