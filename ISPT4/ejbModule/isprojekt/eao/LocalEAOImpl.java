500e8c7e8d35a47c49188c4a86bce9171b41fcf8
wengle <wengle@live.se>
2016-03-01 13:00:46 +0100

    Session beans Local

---
+++ ISPT4/ejbModule/isprojekt/eao/LocalEAOImpl.java
@@ -0,0 +1,18 @@
+package isprojekt.eao;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class LocalEAOImpl
+ */
+@Stateless
+public class LocalEAOImpl implements LocalEAOLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public LocalEAOImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
