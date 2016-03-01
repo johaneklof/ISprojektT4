4ed866a278c8de1515ec6f69384b97d65ae098b0
johaneklof <ekloof@gmail.com>
2016-03-01 09:19:09 +0100

    Pushipush

--- ISPT4/ejbModule/isprojekt/ejb/Location.java
+++ ISPT4/ejbModule/isprojekt/ejb/Location.java
@@ -1,32 +1,36 @@
-import java.util.ArrayList;
 package isprojekt.ejb;
+
+import java.util.ArrayList;
 
 public class Location {
 
-	ArrayList dateList = new ArrayList(); 
+	ArrayList dateList = new ArrayList();
 	private String name;
 	private String address;
 	private String country;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
-	
-	
+
 }
