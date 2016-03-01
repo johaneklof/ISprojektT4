fb52270e4240a4bfc72e8d088e92dfc5a250bcae
johaneklof <ekloof@gmail.com>
2016-03-01 12:19:55 +0100

    Mappat DateAcitivity

--- ISPT4/ejbModule/isprojekt/ejb/DateActivity.java
+++ ISPT4/ejbModule/isprojekt/ejb/DateActivity.java
@@ -1,41 +1,65 @@
 package isprojekt.ejb;
 
-public class DateActivity {
-private String name;
-private String range;
-private int cost;
-private int romantic;
-private int adrenaline;
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getRange() {
-	return range;
-}
-public void setRange(String range) {
-	this.range = range;
-}
-public int getCost() {
-	return cost;
-}
-public void setCost(int cost) {
-	this.cost = cost;
-}
-public int getRomantic() {
-	return romantic;
-}
-public void setRomantic(int romantic) {
-	this.romantic = romantic;
-}
-public int getAdrenaline() {
-	return adrenaline;
-}
-public void setAdrenaline(int adrenaline) {
-	this.adrenaline = adrenaline;
-} 
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DateActivity")
+public class DateActivity implements Serializable {
+	private String name;
+	private String range;
+	private int cost;
+	private int romantic;
+	private int adrenaline;
+
+	@Id
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "range")
+	public String getRange() {
+		return range;
+	}
+
+	public void setRange(String range) {
+		this.range = range;
+	}
+
+	@Column(name = "cost")
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	@Column(name = "romantic")
+	public int getRomantic() {
+		return romantic;
+	}
+
+	public void setRomantic(int romantic) {
+		this.romantic = romantic;
+	}
+
+	@Column(name = "adrenaline")
+	public int getAdrenaline() {
+		return adrenaline;
+	}
+
+	public void setAdrenaline(int adrenaline) {
+		this.adrenaline = adrenaline;
+	}
 
 }
