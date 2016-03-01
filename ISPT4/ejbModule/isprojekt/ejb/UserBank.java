1c4a5ad3e6a1fd935e123cdcfee943bba758c397
wengle <wengle@live.se>
2016-03-01 09:23:09 +0100

    Lista i userBank

--- ISPT4/ejbModule/isprojekt/ejb/UserBank.java
+++ ISPT4/ejbModule/isprojekt/ejb/UserBank.java
@@ -1,5 +1,18 @@
 package isprojekt.ejb;
+import java.util.ArrayList;
 
 public class UserBank {
 
+	ArrayList userList = new ArrayList();
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
 }
