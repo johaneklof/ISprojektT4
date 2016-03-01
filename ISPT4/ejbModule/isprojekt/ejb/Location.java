0b73a1c04d6a8e144c8cd30341aa539e54672fa2
johaneklof <ekloof@gmail.com>
2016-03-01 12:47:29 +0100

    Kopplat samman dateactivity och location

--- ISPT4/ejbModule/isprojekt/ejb/Location.java
+++ ISPT4/ejbModule/isprojekt/ejb/Location.java
@@ -2,10 +2,13 @@ package isprojekt.ejb;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +18,7 @@ public class Location implements Serializable {
 	private String name;
 	private String address;
 	private String country;
+	private Set<DateActivity> dateActivity;//Location can have many dateactivities
 
 	@Id
 	@Column(name = "name")
@@ -42,6 +46,14 @@ public class Location implements Serializable {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+	
+	@OneToMany(mappedBy="location", fetch=FetchType.EAGER)
+	public Set<DateActivity> getDateActivity(){
+		return dateActivity;
+	}
+	public void setDateActivity(Set<DateActivity> dateActivity){
+		this.dateActivity = dateActivity;
 	}
 
 }
